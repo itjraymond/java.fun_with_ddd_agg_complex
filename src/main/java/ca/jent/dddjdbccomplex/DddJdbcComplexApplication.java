0252af3504b00aae75bbac6e969fe2be4e3a90ed
Jocelyn Raymond <it.jraymond@gmail.com>
2023-07-15 13:47:04 -0500

    Initial spring boot project setup

---
+++ src/main/java/ca/jent/dddjdbccomplex/DddJdbcComplexApplication.java
@@ -0,0 +1,13 @@
+package ca.jent.dddjdbccomplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddJdbcComplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddJdbcComplexApplication.class, args);
+	}
+
+}
